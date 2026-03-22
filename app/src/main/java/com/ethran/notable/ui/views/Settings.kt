757e080cb228cfd9978abb623d840492324e4df2
Wiktor Wichrowski <wiktorw39@gmail.com>
2026-03-22 21:58:58 +0100

    add dark mode preview for Settings

--- app/src/main/java/com/ethran/notable/ui/views/Settings.kt
+++ app/src/main/java/com/ethran/notable/ui/views/Settings.kt
@@ -3,6 +3,7 @@ package com.ethran.notable.ui.views
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -429,8 +430,12 @@ fun SettingsPreviewDebug() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
+@Preview(name = "Light", showBackground = true)
+@Preview(
+    name = "Dark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)@Composable
 fun SettingsPreviewSync() {
     InkaTheme {
         SettingsContent(
